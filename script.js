7e6ba6b89d41222474e41e130e689cfaa4f7e4a1
Carrie Young <carrieky04@gmail.com>
2022-10-13 20:54:32 -0700

    adds almost working js code

--- script.js
+++ script.js
@@ -2,10 +2,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numeric = "1234567890"
-var specChar = "!@#$%^&*()_+-={}|[]\:<>?,./~`";
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -17,17 +14,13 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 ///////////////////////// DO NOT CHANGE ABOVE HERE /////////////////////////
 
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var num = ["0","1","2","3","4","5","6","7","8","9"];
-  var password = "";
-  var getLength = "";
 
-
-  // var randUpper = upperCase[Math.floor(Math.random() * upperCase.length)];
-  // var randLower = lowerCase[Math.floor(Math.random() * lowerCase.length)];
-  // var randNum = num[Math.floor(Math.random() * num.length)];
-  // var randSpecialChar = specialChar[Math.floor(Math.random() * specialChar.length)];
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "1234567890"
+var specialChar = "!@#$%^&*()_+-={}|[]\:<>?,./~`";
+var password = "";
+var getLength = "";
 
   
 document.querySelector("#generate").addEventListener("click", generatePassword);
@@ -49,17 +42,17 @@ function generatePassword() {
     if (useSpecialChar === true) {
       password += specialChar;
     }
-    return (password,getLength);
+    // return (password,getLength);
+  var passwordText="";
+    for (var i = 0; i < getLength; i++) {
+      passwordText += password[Math.floor(Math.random() * password.length)];
+      console.log(Math.floor(Math.random() * password.length));
+      console.log(passwordText);
+    }
+      return passwordText;
   } 
   generatePassword();
 
-  for (var i = 0; i < getLength; i++) {
-    
-  }
 
 
 
-  // for (var i=0; i < pwLength.length; i=i+1) {
-  //   password = pwLength(upperCase[randUpper] + lowerCase[randLower] + num[randNum] + specialChar[randSpecialChar]);
-  //   console.log(password);
-  //  }
